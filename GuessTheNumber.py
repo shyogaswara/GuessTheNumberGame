81a2e96c38e20b0c4ff672e2acf5d05af9654b18
shyogaswara <91866594+shyogaswara@users.noreply.github.com>
2022-05-25 15:22:05 +0700

    Add files via upload

---
+++ GuessTheNumber.py
@@ -0,0 +1,56 @@
+'''
+GUESS THE NUMBER MULTIPLAYER
+Created on Mei 25, 2022
+
+@author: Yogaswara
+'''
+
+
+'''
+Criteria :
+1. First player give range to game for Second player to guess
+2. Game give a clue if guess is higher or lower
+3. Win Messages with how many time the player take guess
+4. Ask player to continue or stop playing
+'''
+
+from random import randint
+
+
+def give_range():
+    _start = int(input('starting number : '))
+    _end = int(input('last number : '))
+    _value = randint(_start,_end)
+
+    return _start, _end, _value
+
+def getting_guess():
+    _guess = int(input('guess the number : '))
+    return _guess
+
+def getting_clues(guess,value):
+    if guess > value:
+        print('the number is lower!')
+    elif guess < value:
+        print('the number is higher!')
+
+def main():
+    start, end, value = give_range()
+    guess = None
+    _count = 0
+
+    while guess != value:
+        guess = getting_guess()
+        getting_clues(guess,value)
+        _count += 1
+
+    print(f'''Congrats, The number you're guessing is correct after {_count} times guess''')
+
+
+if __name__ == '__main__':
+    run = True
+    while run:
+        main()
+        _run = input('Again? (y/n)     ')
+        if _run != 'y':
+            run = False
